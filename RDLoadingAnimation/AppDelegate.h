c3a4c4ab0e2f3735c9f37f8cbc07e283766ba81e
Ryan D'souza <dsouzarc@gmail.com>
2015-05-30 16:59:05 -0400

    First commit

---
+++ RDLoadingAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RDLoadingAnimation
+//
+//  Created by Ryan D'souza on 5/30/15.
+//  Copyright (c) 2015 Ryan D'souza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
